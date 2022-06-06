f554237df5c4e9e541e26689beb43b5e0363a12d
kvoloshenko <kvoloshenko@gmail.com>
2022-06-06 14:04:23 +0300

    Implemented user model

--- kvblog/blogapp/models.py
+++ kvblog/blogapp/models.py
@@ -1,8 +1,22 @@
 from django.db import models
+from usersapp.models import BlogUser
 
 # Create your models here.
-class Hh_Request(models.Model):
+class TimeStamp(models.Model):
+    """
+    Abstract - для нее не создаются новые таблицы
+    данные хранятся в каждом наследнике
+    """
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Hh_Request(TimeStamp):
     keywords = models.TextField(blank=False)
+    # user = models.ForeignKey(BlogUser, on_delete=models.CASCADE)
+    user = models.ForeignKey(BlogUser, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.keywords
--- kvblog/kvblog/urls.py
+++ kvblog/kvblog/urls.py
@@ -20,7 +20,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('blogapp.urls', namespace='blog'))
+    path('', include('blogapp.urls', namespace='blog')),
+    path('users/', include('usersapp.urls', namespace='users'))
 ]
 
 # if settings.DEBUG:
